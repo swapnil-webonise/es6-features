e0e0a2c84bee99363331bcb6a5f50fa730c9cd4f
Swapnil Patil <swapnil.patil@weboniselab.com>
2016-07-01 17:48:18 +0530

    Using let variables

--- build/main.bundle.js
+++ build/main.bundle.js
@@ -1,1 +1,10 @@
 "use strict";
+
+var calculateMonthlyPayment = function calculateMonthlyPayment(principal, years, rate) {
+    var monthlyRate = 0;
+    if (rate) {
+        monthlyRate = rate / 100 / 12;
+    }
+    var monthlyPayment = principal * monthlyRate / (1 - Math.pow(1 / (1 + monthlyRate), years * 12));
+    return monthlyPayment;
+};
